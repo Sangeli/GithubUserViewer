b7db9819381da54a9b42feb199cbb10ddd16931f
Stephen Cefali <stephencefali@gmail.com>
2017-01-08 13:45:07 -0800

    testing change for heroku

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.listen(port, () => console.log('server up and running at %s port', port));
 // Add headers
 app.use( (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
